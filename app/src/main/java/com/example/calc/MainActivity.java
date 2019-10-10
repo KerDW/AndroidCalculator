fa8db7cf983d129dacee9d79b253fb8dce0f0680
Álvaro Zamora <alvarozajim@gmail.com>
2019-10-10 18:59:01 +0200

    better basic layout and numbers

--- app/src/main/java/com/example/calc/MainActivity.java
+++ app/src/main/java/com/example/calc/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.calc;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,7 +15,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void numberStroke(){
+    public void numberStroke(View v){
+
+        String press = v.getTag().toString();
+        TextView  number1 = (TextView) findViewById(R.id.number1);
+        TextView  number2 = (TextView) findViewById(R.id.number2);
+        TextView  op = (TextView) findViewById(R.id.operator);
+
+        if(op.getText() == ""){
+            number1.setText(number1.getText()+press);
+
+
+        }
 
 
     }
