4c50976331730085a53ed2028a31423cbd60b4c4
Álvaro Zamora <alvarozajim@gmail.com>
2019-10-13 13:22:11 +0200

    open actual calculator

--- app/src/main/java/com/example/calc/MainActivity.java
+++ app/src/main/java/com/example/calc/MainActivity.java
@@ -2,9 +2,9 @@ package com.example.calc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -131,6 +131,13 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+    }
+
+    public void realCalc(View v){
+        Intent launchIntent = getPackageManager().getLaunchIntentForPackage("com.miui.calculator");
+        if (launchIntent != null) {
+            startActivity(launchIntent);
+        }
     }
 
 }
